c05e33d12b93fc240f315a0d9e0ff77b8f3f0f07
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-02 23:33:43 +0900

    ✨ 画像データをテキストデータに変換 (#3)

--- investigation/tessract_pytesseract/tesseract_pytesseract.py
+++ investigation/tessract_pytesseract/tesseract_pytesseract.py
@@ -5,6 +5,11 @@ import cv2
 import glob
 import numpy as np
 
+import pytesseract
+
+
+LANG = "eng+jpn"
+
 
 def preprocessing(image_path: str) -> Image.Image:
     """
@@ -21,6 +26,13 @@ def preprocessing(image_path: str) -> Image.Image:
     return img
 
 
+def get_text(image: Image.Image) -> str:
+    """
+    画像データをtextに変換
+    """
+    return pytesseract.image_to_string(image, lang=LANG)
+
+
 def scan(fp: str, out_dir: str) -> None:
     receipt_source_name = fp.split("/")[-1].replace(".jpeg", "")
     output_file_name = f"{out_dir}/{receipt_source_name}"
@@ -29,6 +41,11 @@ def scan(fp: str, out_dir: str) -> None:
     preprocessed_image = preprocessing(fp)
     preprocessed_fp = f"{output_file_name}_preprocessed.png"
     preprocessed_image.save(preprocessed_fp)
+
+    # textデータに変換
+    text = get_text(preprocessed_image)
+    with open(f"{output_file_name}.txt", "w") as f:
+        f.writelines(text)
 
 
 def main() -> None:
