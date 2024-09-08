4b57bcba5031b284d9c03045764fc44d7c6f5873
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-08 18:58:12 +0900

    ✅ レシートのfpを返すapiのテストを追加 (#18)

--- tests/test_api/test_main.py
+++ tests/test_api/test_main.py
@@ -6,6 +6,25 @@ client = TestClient(app)
 
 
 def test_root():
+    """
+    APIのバージョンを確認するテスト
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"version": "0.1.0"}
+
+
+def test_post_receipt_analyze():
+    """
+    レシートのfpを確認するテスト
+    """
+    response = client.post(
+        "/receipt-analyze",
+        json={
+            "file": "/Users/ayumu/my-projects/receipt-scanner-model/raw/book-off.jpeg"
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "total": "/Users/ayumu/my-projects/receipt-scanner-model/raw/book-off.jpeg"
+    }
