f9eb34e86bf97a04e4bb6609aa3ed82f0fa36dc4
AyumuOgasawara <ogasaayumu0114@gmail.com>
2024-09-08 18:56:09 +0900

    👽️ レシートを解析する機能をapiで叩けるようにした (#18)

--- api/main.py
+++ api/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+from src.receipt_scanner_model import analyze
+from pydantic import BaseModel
 import tomllib
+
+
+# TODO: fileの型をbytesに変更する
+class Receipt(BaseModel):
+    file: str
 
 
 with open("pyproject.toml", "rb") as f:
@@ -15,3 +22,12 @@ async def root():
     APIのバージョンを返す
     """
     return {"version": app.version}
+
+
+@app.post("/receipt-analyze")
+async def receipt_analyze(receipt: Receipt):
+    """
+    レシートの合計を返す
+    """
+    total = analyze.main(receipt.file)
+    return {"total": total}
