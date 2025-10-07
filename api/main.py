4f98079719e89f6b1a80d02a4f3b4612bcf336fc
AyumuOgasawara <ogasaayumu0114@gmail.com>
2025-10-08 01:56:23 +0900

    🥅 リクエスとの400と415のハンドリングを追加 (#87)

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from src.receipt_scanner_model.analyze import ReceiptDetail, get_receipt_detail
@@ -46,9 +46,33 @@ async def custom_exception_handler(request, exc: CustomHTTPException):
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc: RequestValidationError):
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
     """RequestValidationErrorをHTTPExceptionの形に変換する"""
     logger.exception("レシート解析中にエラーが起きました。")
+
+    # Content-Typeチェック
+    content_type = request.headers.get("content-type", "")
+    if not content_type.startswith("application/json"):
+        return JSONResponse(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            content={
+                "error_type_code": ErrorCode.CLIENT_ERROR.value,
+                "message": "リクエストのContent-Typeが不正です。",
+            },
+        )
+
+    error_type = exc.errors()[0]["type"]
+
+    # フィールドの欠落やJSONの不正な場合は400 Bad Requestを返す
+    if error_type in ["missing", "json_invalid"]:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "error_type_code": ErrorCode.CLIENT_ERROR.value,
+                "message": "リクエストが不正です。",
+            },
+        )
+
     raise CustomHTTPException(
         http_status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         error_type_code=ErrorCode.CLIENT_ERROR,
